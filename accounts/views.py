a25c45671374f15b00466c2c6247f1a18f43a1cd
Josephpaik <josephpaik2008@gmail.com>
2018-04-28 21:44:13 +0900

    part 6 내 프로필 변경추가, 메시지 마크다운 기능 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,23 @@
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import UpdateView
 
 from .forms import SignUpForm
+
+
+@method_decorator(login_required, name='dispatch')
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ('last_name', 'first_name', 'email', )
+    template_name = 'my_account.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self, queryset=None):
+        return self.request.user
 
 
 def signup(request):
@@ -16,4 +32,5 @@ def signup(request):
         # form = UserCreationForm()
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
 
--- quiz_board/urls.py
+++ quiz_board/urls.py
@@ -22,13 +22,11 @@ from accounts import views as accounts_views
 
 urlpatterns = [
     # index page
-    # path('', views.home, name='home'),
     path('', views.BoardListView.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('admin/', admin.site.urls),
 
     # board_topics
-    # path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/', views.TopicListView.as_view(), name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
 
@@ -76,4 +74,6 @@ urlpatterns = [
     path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(
         template_name='password_change_done.html'),
          name='password_change_done'),
+    path('settings/account/', accounts_views.UserUpdateView.as_view(),
+         name='my_account'),
 ]
